75e434e7a5d1d12357f92c5b7bbb8edf394debde
SashanthSajith <81414661+SashanthSajith@users.noreply.github.com>
2021-10-20 19:40:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+paddleX = 10;
+paddleY = 685;
+game_status = "";
 
-/*created by prashant shukla */
+
+/*created by Sashanth Sajith */
 
 var paddle2 =10,paddle1=10;
 
@@ -22,9 +26,30 @@ var ball = {
 }
 
 function setup(){
-  var canvas =  createCanvas(700,600);
-}
+    var canvas =  createCanvas(700,600);
+   canvas.parent('canvas');
 
+	video = createCapture(VIDEO);
+	video.size(700,600);
+  video.hide();
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose',gotPoses);
+}
+function modelLoaded()
+{
+  console.log('modelLoaded');
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+	{
+		rightWristY = results[0].pose.rightWrist.y; 
+    rightWristX = results[0].pose.rightWrist.x; 
+    scoreRightWrist = results[0].pose.keypoints[10].score; 
+    console.log(scoreRightWrist);
+	}
+}
 
 function draw(){
 
@@ -161,4 +186,9 @@ function paddleInCanvas(){
   if(mouseY < 0){
     mouseY =0;
   }  
+}
+function startGame()
+{
+  game_status = "start";
+  document.getElementById("status").innerHTML = "Game Is Loaded ";
 }
